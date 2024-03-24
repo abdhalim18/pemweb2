2c468ba8357738c8af5b4cebf967c508407cdd60
abdhalim18 <mabdulhalim257@gmail.com>
2024-03-24 21:16:33 +0700

    create layout with templating file

---
+++ pertemuan 5/ci4/app/Views/page/about.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur consequatur cumque ab laudantium iusto
+                recusandae incidunt aut ratione? Officia odio quo voluptates. Distinctio, blanditiis minima voluptatem
+                delectus ducimus doloremque fugiat.</h3>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
